574a157cd45646894c399f8eac905befb551fec6
Daniel <brikbusters@gmail.com>
2022-11-26 13:49:50 -0500

    Update ptpview.c

--- ptpview.c
+++ ptpview.c
@@ -9,6 +9,8 @@
 #include <ptp.h>
 #include <imgconv.h>
 
+// DIGIC 4/5 only
+
 #define WIDTH 720
 #define HEIGHT 480
 
@@ -17,7 +19,9 @@
 #endif
 
 #define RGB_SIZE ((WIDTH * HEIGHT * 3) / 4)
-#define BUF_SIZE ((RGB_SIZE / 6))
+
+// Seems to have no difference
+#define BUF_SIZE ((RGB_SIZE / 1))
 
 void ptpview_task() {}
 
@@ -34,17 +38,25 @@ uint8_t *rgb = NULL;
 uint8_t *bmp_copy = NULL;
 uint32_t *yuv_copy = NULL;
 
+uint8_t *bvram = NULL;
+uint8_t *lvram = NULL;
+
 int get_screenscreen(struct ptp_context *data)
 {
 	/* what to save? */
 	int save_yuv = 1;
 	int save_bmp = 1;
-	
-	uint8_t *bvram = bmp_vram();
-	uint8_t *lvram = NULL;
-	struct vram_info *vram_info = get_yuv422_vram();
 
-	if (vram_info != NULL) lvram = vram_info->vram;
+	if (bvram == NULL) {
+		bvram = bmp_vram();
+	}
+
+	if (save_yuv) {
+		if (lvram == NULL) {
+			struct vram_info *vram_info = get_yuv422_vram();
+			if (vram_info != NULL) lvram = vram_info->vram;
+		}
+	}
 
 	/* can we save the YUV buffer? (it might be uninitialized, e.g. in photo mode before going to LV) */
 	if (!lvram) save_yuv = 0;
@@ -79,9 +91,9 @@ int get_screenscreen(struct ptp_context *data)
 				p = (int)bvram[BM(x, y)];
 				
 				/* get palette entry (including our DIGIC pokes, if any) */
-				pal = shamem_read(LCD_Palette[3*p]);
-				if (!pal)
-					pal = LCD_Palette[3*p + 2];
+				//pal = shamem_read(LCD_Palette[3*p]);
+				//if (!pal)
+					pal = LCD_Palette[3 * p + 2];
 				opacity = (pal >> 24) & 0xFF;
 				Y = (pal >> 16) & 0xFF;
 				U = (pal >>  8) & 0xFF;
@@ -93,7 +105,11 @@ int get_screenscreen(struct ptp_context *data)
 			uint32_t uyvy = 0;
 
 			/* handle transparency (incomplete, needs more reverse engineering) */
-			if (pal == 0x00FF0000) /* fully transparent */
+			#if 1
+			if (pal == 0x00FF0000 && !save_yuv) {
+				// asdasdsa
+			}
+			else if (pal == 0x00FF0000) /* fully transparent */
 			{
 				if (save_yuv)
 				{
@@ -118,20 +134,20 @@ int get_screenscreen(struct ptp_context *data)
 				V = ((int)V + (int)V2) / 2;
 			}
 			/* other transparency codes? */
-			
+			#endif
 			/* convert to RGB */
-			int R,G,B;
-			yuv2rgb(Y, U, V, &R, &G, &B);
+			int R, G, B;
+			//yuv2rgb(Y, U, V, &R, &G, &B);
 			
 			/* copy to our buffer */
-			rgb[read + 0] = R;
-			rgb[read + 1] = G;
-			rgb[read + 2] = B;
+			rgb[read + 0] = Y;
+			rgb[read + 1] = U;
+			rgb[read + 2] = V;
 			read += 3;
 
 			if (read == BUF_SIZE) {
 				if (data->send_data(data->handle, (void *)rgb, BUF_SIZE, tmpsize, 0, 0, 0)) {
-					return -24;
+					return -2;
 				}
 
 				read = 0;
@@ -154,8 +170,15 @@ static int ptpview_handler(void *priv, struct ptp_context *context,
 						   uint32_t param1, uint32_t param2, uint32_t param3, uint32_t param4, uint32_t param5)
 {
 	int r = get_screenscreen(context);
+	
+	//int tmpsize = RGB_SIZE;
+	//for (int i = 0; i < 6; i++) {
+	//	context->send_data(context->handle, (void*)rgb, BUF_SIZE, tmpsize, 0, 0, 0);
+	//	tmpsize = 0;
+	//}
+	
 	if (r) {
-		bmp_printf(FONT_CANON, 10, 10, "ptpview screenshot err %d", r);
+		bmp_printf(FONT_CANON, 10, 10, "ptpview err %d", r);
 	}
 
 	struct ptp_msg msg = {
@@ -195,4 +218,3 @@ MODULE_INFO_START()
 MODULE_INIT(ptpview_init)
 MODULE_DEINIT(ptpview_deinit)
 MODULE_INFO_END()
-
